1c259cf1cb3b061641194f149f958a81722d4095
Derek Stegelman <dstegelman@gmail.com>
2018-01-23 08:25:14 -0700

    Include new sizes

--- resize.py
+++ resize.py
@@ -1,14 +1,14 @@
 from PIL import Image
 from resizeimage import resizeimage
 
-IMAGE_SIZES = [36, 48, 72, 96, 144, 192, 180, 60, 120, 76, 152, 40, 80, 57, 114, 167, 29, 58, 50, 100]
-IMAGE_FILE_NAME = 'fruit-square.png'
-IMAGE_NAME = 'fruit-square'
+IMAGE_SIZES = [32, 16, 196, 36, 48, 72, 96, 144, 192, 180, 60, 120, 76, 152, 40, 80, 57, 114, 167, 29, 58, 50, 100]
+IMAGE_FILE_NAME = 'icon.png'
+IMAGE_NAME = 'icon-'
 
 for img_size in IMAGE_SIZES:
     fd_img = open(IMAGE_FILE_NAME, 'r')
     img = Image.open(fd_img)
     img = resizeimage.resize_thumbnail(img, [img_size, img_size])
-    img.save(IMAGE_NAME + str(img_size) + '-.png', img.format)
+    img.save(IMAGE_NAME + str(img_size) + '.png', img.format)
     fd_img.close()
     print('Resized ' + str(img_size))
